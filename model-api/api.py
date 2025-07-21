bbf1e95922f49b0cb046b3945ba923049a3ef033
woosung <woosung5968@gmail.com>
2025-07-21 11:08:35 +0000

    fix(api): data 수정

--- model-api/api.py
+++ model-api/api.py
@@ -34,7 +34,7 @@ class FinalPromptRequest(BaseModel):
 @app.post("/api/generate-prompt")
 async def generate_prompt_endpoint(request: PromptRequest):
     #data = await request.json()
-    print("[실제 전달받은 요청 JSON]", data)
+    #print("[실제 전달받은 요청 JSON]", data)
     print(f"[REQUEST] POST /api/generate-image")
     print(f"[DATA] category: {request.category}")
     print(f"[DATA] layer: {request.layer}")
