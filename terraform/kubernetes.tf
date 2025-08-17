6af6d763c29ca32f1d559fd6c608d841eb4501e6
woosung <woosung5968@gmail.com>
2025-08-17 14:32:35 +0000

    feat(terraform): Git의 Client/Tenant ID 노출 문제 해결 및 K8s 리소스 직접 관리 기능 추가

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,17 @@
+resource "kubernetes_namespace" "loki" {
+    metadata {
+        name        = "loki"
+    }
+}
+
+resource "kubectl_manifest" "loki_spc" {
+  yaml_body = templatefile("${path.module}/../k8s/Loki/secret-provider-class.yaml.tftpl", {
+    client_id = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].client_id
+    tenant_id = data.azurerm_client_config.current.tenant_id
+  })
+
+    depends_on = [
+        kubernetes_namespace.loki,
+        azurerm_role_assignment.aks_kubelet_kv_reader
+    ]
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,9 +4,24 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "kubernetes" {
+  host                  = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
 }
