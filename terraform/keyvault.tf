89d713a6abb1ce8afeeeac9b618f10a00793cfaf
woosung <woosung5968@gmail.com>
2025-08-17 14:27:19 +0000

    refactor(terraform): Key Vault 권한을 RBAC로 현대화

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -5,6 +5,8 @@ resource "azurerm_key_vault" "main" {
     resource_group_name = azurerm_resource_group.aks_rg.name
     tenant_id           = data.azurerm_client_config.current.tenant_id
     sku_name            = "standard"
+
+    enable_rbac_authorization = true
 }
 
 resource "azurerm_key_vault_secret" "storage_account_key" {
@@ -13,27 +15,14 @@ resource "azurerm_key_vault_secret" "storage_account_key" {
     value               = azurerm_storage_account.Loki.primary_access_key
 }
 
-resource "azurerm_key_vault_access_policy" "terraform_user_policy" {
-    key_vault_id       = azurerm_key_vault.main.id
-    tenant_id          = data.azurerm_client_config.current.tenant_id
-    object_id          = data.azurerm_client_config.current.object_id
-
-    secret_permissions = [
-        "Get",
-        "List",
-        "Set",
-        "Delete",
-        "Purge"
-    ]
+resource "azurerm_role_assignment" "terraform_user_kv_admin" {
+    scope               = azurerm_key_vault.main.id
+    role_definition_name= "Key Vault Secrets Officer"
+    principal_id        = data.azurerm_client_config.current.object_id 
 }
 
-resource "azurerm_key_vault_access_policy" "aks_policy" {
-    key_vault_id        = azurerm_key_vault.main.id
-    tenant_id           = data.azurerm_client_config.current.tenant_id
-    object_id           = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
-
-    secret_permissions = [
-        "Get",
-        "List"
-    ]
+resource "azurerm_role_assignment" "aks_kubelet_kv_reader" {
+    scope               = azurerm_key_vault.main.id
+    role_definition_name= "Key Vault Secrets User"
+    principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
 }
