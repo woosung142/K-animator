6bcd0895c294cfef2dec453dec583b3822bcf0b4
woosung <woosung5968@gmail.com>
2025-08-29 12:10:45 +0000

    fix: git Repository 이동

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -6,12 +6,12 @@ resource "kubernetes_namespace" "loki" {
 
 resource "kubectl_manifest" "loki_spc" {
   yaml_body = templatefile("${path.module}/../k8s/charts/Loki/secret-provider-class.yaml.tftpl", {
-    client_id = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].client_id
+    client_id = azurerm_user_assigned_identity.loki.client_id
     tenant_id = data.azurerm_client_config.current.tenant_id
   })
 
     depends_on = [
         kubernetes_namespace.loki,
-        azurerm_role_assignment.aks_kubelet_kv_reader
+        azurerm_role_assignment.loki_identity_kv_reader
     ]
 }
