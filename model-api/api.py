28414aa7d6de8e5138286ffed353f7095475d3fb
woosung <woosung5968@gmail.com>
2025-08-31 17:01:37 +0000

    fix: 누락된 라이브러리 수정

--- model-api/api.py
+++ model-api/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from celery import Celery
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import PrometheusInstrumentator
 import os
 import logging
 
